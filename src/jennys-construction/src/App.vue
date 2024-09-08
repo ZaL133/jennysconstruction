c41d057c46a558e7198642d05c6f49e286ba2ae8
Zeph <zeph.levin@gmail.com>
2024-09-08 06:54:02 -0400

    Trying to get the theme working

--- src/jennys-construction/src/App.vue
+++ src/jennys-construction/src/App.vue
@@ -17,7 +17,7 @@
 <template>
   <v-app>
     <!-- App Bar -->
-    <v-app-bar app>
+    <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon class="jc-mobile-menu" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>
         <v-btn text href="#">Jenny's Construction</v-btn>
@@ -63,7 +63,7 @@
     </v-main>
 
     <!-- Footer -->
-    <v-footer app padless>
+    <v-footer app padless color="secondary">
       <v-col class="text-center">
         © 2024 Jenny's Construction
       </v-col>
--- src/jennys-construction/src/plugins/vuetify.js
+++ src/jennys-construction/src/plugins/vuetify.js
@@ -5,6 +5,19 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  theme: {
+  defaultTheme: 'light',
+  themes: {
+      light: {
+        primary: '#1ecbe1',
+        secondary: '#424242', // Dark grey for contrast
+        accent: '#82B1FF', // Light blue for subtle accents
+        error: '#FF5252', // Red for errors or alerts
+        info: '#2196F3',  // Standard info blue
+        success: '#4CAF50', // Green for success messages
+        warning: '#FFC107', // Yellow for warnings
+      },
+    },
+  },
+});
